100f10c04342e954c2177125edacbb2d7584f418
jabongg <jbpvns@gmail.com>
2017-07-20 21:35:55 +0530

    adding server.js

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 // get the packages we need ============
 // =======================
 'use strict'
+
 var express = require('express');
-var app     = express();
-var bodyParser  = require('body-parser');
+var bodyParser = require('body-parser');
+
 var morgan      = require('morgan');
 var mongoose    = require('mongoose'); // used to handle queries related to database
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
@@ -12,76 +13,27 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
 var User   = require('./app/models/user'); // get our mongoose model
 
-// =======================
-// configuration =========
-// =======================
-var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
+var app = express();
+
+
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
 mongoose.connect(config.database);  // connect to database
 app.set('superSecret',config.secret);  //secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended : false  }));
+app.use(bodyParser.urlencoded({ extended : true  }));
 app.use(bodyParser.json());
-
-// use morgan to log requests to the console
-app.use(morgan('dev'));
-
-
-// =======================
-// routes ================
-// =======================
-// basic route
-app.get('/', function(request, result) {
-  result.send("Node is the base to create great apps on port :" + port);
-});
-
 
 // API ROUTES -------------------
 
 // get an instance of the router for api routes
-var apiRoutes  = express.Router();
-
-
-// TODO: route middleware to verify a token
-
-// route to show a random message (GET http://localhost:8080/api/)
-apiRoutes.get('/', function (request, result) {
-  result.json({ message : 'welcome to writealone!'});
-});
-
-// route to return all users (GET http://localhost:8080/api/users)
-apiRoutes.get('/users', function (request, result) {
-  User.find({}, function(error, users) {
-    result.send(users);
-  });
-});
-
-// apply the routes to our application with the prefix /api
-app.use('/api', apiRoutes);
-
-app.get('/setup', function(request, result) {
-   // create a sample user
-   var mike = new User({
-     name      : 'jb',
-     password  : 'jb',
-     admin     : true
-   });
-
-    // save the sample user
-    mike.save(function(error) {
-      if (error) {
-        throw error;
-      }
-
-      console.log('user saved successfully in db');
-      result.json({ success : true });
-    });
-});
-
+//var apiRoutes  = express.Router();
 
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
-apiRoutes.post('/authenticate', function(req, res) {
-
+//apiRoutes.post('/authenticate', function(req, res) {
+app.post('/authenticate', function(req, res) {
 console.log('req.body.name' + req.body.name);
 console.log('req.body.password' + req.body.password);
 
@@ -92,8 +44,7 @@ console.log('response' + res);
   User.findOne({
     name: req.body.name
   }, function(err, user) {
-
-    if (err) throw err;
+     (err) throw err;
 
     if (!user) {
       console.log(user);
@@ -126,8 +77,18 @@ console.log('response' + res);
 
 
 // =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
+
+// =======================
 // start the server ======
 // =======================
 
+// listen on port
 app.listen(port);
+
 console.log('Magic happens at http://localhost:' + port);
+
+
+module.exports = app;
