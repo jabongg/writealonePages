88852184cfd04dd59ea4ef3c29ede97c831a54e1
Jang Bahadur <jang@SIPL.WORK>
2017-07-20 14:30:14 +0530

    adding node modulse for mongoclient

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // connecting to mongodb database
 module.exports = {
   'secret': 'writealone@2017',
-  'database': 'mongodb://localhost:27017/writalone'
+//  'database': 'mongodb://jangu:mongolab_2016@ds145380.mlab.com:45380/writealone'
+  'database':  'mongodb://localhost:27017/writalone'
 }
